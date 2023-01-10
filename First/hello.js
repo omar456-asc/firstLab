cd7ce26d044983f931653f3787245ffd22cd978a
Omar Walid <omo_1712@yahoo.com>
2023-01-10 13:28:15 +0200

    Adding Lab

---
+++ First/hello.js
@@ -0,0 +1,4 @@
+var firstName = prompt("What's your first name?");
+var lastName = prompt("What is your last name?");
+var age = Number(prompt("What is your age"));
+document.write(`Welcome ${firstName} ${lastName} you are ${age} years old`);
---
+++ Second/hello.js
@@ -0,0 +1,5 @@
+alert("This is my prototype calculator that can only add two numbers");
+var firstNum = Number(prompt("Enter First number"));
+var secondNum = Number(prompt("Enter Seconed number"));
+var result = firstNum + secondNum ;
+document.write("The result ="+result);
